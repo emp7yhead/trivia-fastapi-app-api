dcd93a3ffba8523f527954586acf82abe1d3ea24
emp7yhead <artyomkropp@gmail.com>
2022-07-18 21:52:27 +0900

    Add: routes.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
 """Main script for app."""
 from fastapi import FastAPI
 
+from app.routes.question import router
+
 app = FastAPI()
+
+app.include_router(router, prefix='/v1')
---
+++ app/routes/question.py
@@ -0,0 +1,11 @@
+"""Define endpoints for API."""
+
+from fastapi import APIRouter
+
+router = APIRouter(prefix='/question')
+
+
+@router.get('/')
+async def get_all_questions():
+    """Get all questions."""
+    return {'world':'hello'}
