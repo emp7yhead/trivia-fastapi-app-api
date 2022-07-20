b198041ddff47edfcebccf9edf0fedbe0b0a7e14
emp7yhead <artyomkropp@gmail.com>
2022-07-21 01:46:23 +0900

    Add: database module

---
+++ app/database/base.py
@@ -0,0 +1,3 @@
+from app.database.base_class import Base
+from app.models.questions import Question
+from app.models.answers import Answer
---
+++ app/database/db.py
@@ -0,0 +1,8 @@
+"""Configure database."""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.config.settings import settings
+
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
