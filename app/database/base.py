f56e4fe7248aee6f3758e342766bc55258784162
emp7yhead <artyomkropp@gmail.com>
2022-09-11 00:09:04 +0900

    Fix: imports

--- app/database/base.py
+++ app/database/base.py
@@ -1,3 +1,4 @@
 from app.database.base_class import Base
-from app.models.questions import Question
 from app.models.answers import Answer
+from app.models.category import Category
+from app.models.questions import Question
